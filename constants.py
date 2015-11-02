206bd37fbb2e985f6155e77b4d9d3096ff47a17d
Cristian Zanoci <czanoci@gmail.com>
2015-11-01 23:20:31 -0800

    Implemented sampleImage in train_baseline.py

--- constants.py
+++ constants.py
@@ -1,2 +1,8 @@
 # the side length (in pixels) of the nxn square over which we compute the average luminance. Assume it's odd!
 sampling_side = 5;
+
+# number of points to sample from colored image in the X direction
+X_points = 15
+
+# number of points to sample from colored image in the Y direction
+Y_points = 15
--- train_baseline.py
+++ train_baseline.py
@@ -1,6 +1,26 @@
 from constants import *
+from util import *
+import numpy as np
 from skimage import io, color
 
-img_rgb = io.imread('./Images/Landscape/mountain_color.jpg')
+# Given the filename of a colored image, returns an array of sampled points, each containing the [l, a, b, sigma] values
+def sampleImage(colored_filename):
+	img_rgb = io.imread(colored_filename)
+	img_gray = color.rgb2gray(img_rgb)*255
+	img_lab = color.rgb2lab(img_rgb)
+	# integer division
+	delta_x = len(img_rgb)/(X_points + 1)
+	delta_y = len(img_rgb[0])/(Y_points + 1)
+	grid = np.zeros((X_points, Y_points, 4))
+	for i in xrange(X_points):
+		for j in xrange(Y_points):
+			x = (i+1)*delta_x
+			y = (j+1)*delta_y
+			sigma = computeStdDevLuminance(img_gray, x, y)
+			grid[i][j] = np.append(img_lab[x][y], sigma)
+	# reshape the grid into a 1D array
+	return np.reshape(grid, (-1, 4))
 
-img_lab = color.rgb2lab(img_rgb)
+grid = sampleImage('./Images/Landscape/mountain_color.jpg')
+print grid
+
